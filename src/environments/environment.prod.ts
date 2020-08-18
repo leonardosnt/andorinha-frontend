ceca4bd26a4d456ecda2fe7eada3f8e48fe2a038
leonardosnt <leonrdsnt@gmail.com>
2020-08-18 13:41:52 -0300

    Atualiza apiUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // TODO: igual ao do ambiente de desenvolvimento por enquanto.
-  apiUrl: 'http://localhost/andorinha-backend/api'
+  apiUrl: 'http://localhost:8080/andorinha-backend/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost/andorinha-backend/api'
+  apiUrl: 'http://localhost:8080/andorinha-backend/api'
 };
 
 /*
